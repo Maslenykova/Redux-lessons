d9583770710d233910b11c3db2ea41021b2f7af7
Maslenykova <smaslenykova8@gmail.com>
2025-02-14 18:46:29 +0200

    lesson2/task3

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -1,7 +1,10 @@
 import { addUser, deleteUser } from './users.actions';
 import store from './store';
 
-store.dispatch(addUser({ name: "John", id: 100 })); 
-store.dispatch(deleteUser(76)); 
+const newUser = { id: 1, name: 'John Doe' };
 
+store.dispatch(addUser(newUser)); 
+console.log(store.getState()); 
+
+store.dispatch(deleteUser(1)); 
 console.log(store.getState());
--- lesson2/task3/src/users.reducer.js
+++ lesson2/task3/src/users.reducer.js
@@ -2,10 +2,7 @@
 import {ADD_USERS, DELETE_USERS} from './users.actions';
 
 const initialState = {
-    users: [
-        { name: 'Sarah', id: 76 },
-        { name: 'Anna', id: 78 }
-    ]
+    usersList: [],
 };
 
 export const usersReducer = (state = initialState, action)=>{
@@ -13,12 +10,12 @@ export const usersReducer = (state = initialState, action)=>{
         case ADD_USERS:
             return{
                 ...state,
-               users: [...state.users, action.payload]
+                usersList: state.usersList.concat(action.payload),
             }
         case DELETE_USERS:
             return{
                 ...state,
-                users: state.users.filter(user => user.id !== action.payload)
+                usersList: state.usersList.filter(user => user.id !== action.payload),
             }
         default:
             return state;
