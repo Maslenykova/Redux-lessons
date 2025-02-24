27d950f954812610668d7aa3392de239f533a925
Maslenykova <smaslenykova8@gmail.com>
2025-02-24 15:17:08 +0200

    les

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -7,13 +7,13 @@ const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) =
     return (
         <div className="pagination">
             {!isPrevHidden && (
-                <button className='btn' onClick={goPrev}>←</button>
+                <button className="btn" onClick={goPrev}>←</button>
             )}
 
             <span className="pagination__page">{currentPage + 1}</span>
 
             {!isNextHidden && (
-                <button className='btn' onClick={goNext}>→</button>
+                <button className="btn" onClick={goNext}>→</button>
             )}
         </div>
     );
