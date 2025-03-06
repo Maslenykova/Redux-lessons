428bcb31a22d8b9783310774ffae17cc8965765a
Maslenykova <smaslenykova8@gmail.com>
2025-03-06 19:53:21 +0200

    les

--- lesson6/task3/src/weather/weather.action.js
+++ lesson6/task3/src/weather/weather.action.js
@@ -1,4 +1,3 @@
-import { getWeatherData  } from './weather.geteway';
 
 export const CITY_DATA_RECEIVED = 'CITY_DATA_RECEIVED';
 
@@ -9,6 +8,17 @@ export const cityDataReceived = (cityData) =>{
         payload: cityData,
     }
 };
+
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities";
+
+export const getWeatherData = () => {
+    return fetch(baseUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch');
+            }
+            return response.json();
+        })} 
 
 
 export const fetchCityData = () => {
