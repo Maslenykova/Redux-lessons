ad7445a4814c6c10392cf47afcd50352b50fb0f0
Maslenykova <smaslenykova8@gmail.com>
2025-03-06 20:12:35 +0200

    les

--- lesson6/task3/src/weather/weather.action.js
+++ lesson6/task3/src/weather/weather.action.js
@@ -11,45 +11,23 @@ export const cityDataReceived = (cityData) =>{
 
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities";
 
-export const getWeatherData = () => {
-    return async (dispatch) => {
-        try {
-            const response = await fetch(baseUrl);
-
+export const getData = () => {
+    return fetch(baseUrl)
+        .then(response => {
             if (!response.ok) {
-                throw new Error("Failed to fetch city data");
+                throw new Error('Failed to fetch');
             }
+            return response.json();
+        })} 
 
-            const cityData = await response.json();
 
+export const getWeatherData = () => {
+    return function (dispatch) {
+        getData().then(cityData => {
             if (Array.isArray(cityData)) {
                 dispatch(cityDataReceived(cityData));
-            } else {
-                console.error("Invalid city data format:", cityData);
-            }
-        } catch (error) {
-            console.error("Error fetching city data:", error);
-        }
+            } 
+        });
     };
 };
-
-// export const getWeatherData = () => {
-//     return fetch(baseUrl)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Failed to fetch');
-//             }
-//             return response.json();
-//         })} 
-
-
-// export const fetchCityData = () => {
-//     return function (dispatch) {
-//         getWeatherData().then(cityData => {
-//             if (Array.isArray(cityData)) {
-//                 dispatch(cityDataReceived(cityData));
-//             } 
-//         });
-//     };
-// };
 
